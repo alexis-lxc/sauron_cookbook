20c13204387956bf76acbc2c31e26ef93a29d61e
Abhishek Sangwan <abhisheksangwan1994@gmail.com>
2018-07-04 18:17:34 +0530

    add specs for postgres to be installed and enabled

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -24,6 +24,12 @@ describe systemd_service('sidekiq') do
   it { should be_running }
 end
 
+describe systemd_service('postgresql') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
 describe file('/opt/sauron/.config/lxc/client.crt') do
   it { should exist }
 end
